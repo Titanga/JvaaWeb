fd0c92e6975fb872b6cbf91c28680c1d4a009683
obcy <248835053@qq.com>
2019-08-23 21:41:03 +0800

    Initial commit

---
+++ moudle1/src/HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * @auther obcy
+ * @date 2019/8/23 0023 - 21:38
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello Word");
+    }
+}
